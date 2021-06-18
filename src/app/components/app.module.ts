1f64017c8c98c0170d285cd7a356d1a1f631d31b
Esteban <estebanarivasv@gmail.com>
2021-06-18 18:03:16 -0300

    Import HttpClientModule

--- src/app/components/app.module.ts
+++ src/app/components/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,8 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ExchangeComponent } from './exchange/exchange.component';
-import { WalletComponent } from './wallet/wallet.component';
-
+import { WalletComponent } from './dashboard/wallet/wallet.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,6 +19,7 @@ import { WalletComponent } from './wallet/wallet.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
