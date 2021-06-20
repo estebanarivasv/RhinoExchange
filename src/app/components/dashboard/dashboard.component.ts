1fffa88a47c836c3368b35fbf186c46ab9ff547c
Esteban <estebanarivasv@gmail.com>
2021-06-20 12:32:07 -0300

    Reorganizing dashboard layout

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+import { WalletService } from 'src/app/services/wallet.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  // Screen size
+  mediaSubcriptor: Subscription = new Subscription;
+  deviceXs: boolean = false;
 
-  ngOnInit(): void {
+  title = "Dashboard";
+
+
+  constructor(public mediaObserver: MediaObserver, private walletService: WalletService) { }
+
+  ngOnInit() {
+    // Tracks if screen size changes
+    this.mediaSubcriptor = this.mediaObserver.media$.subscribe((result: MediaChange) => {
+      this.deviceXs = result.mqAlias === "xs" ? true : false;
+    })
   }
 
+  ngOnDestroy() {
+    this.mediaSubcriptor.unsubscribe();
+  }
+
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+  addWallet() {
+    console.log("llama")
+    this.walletService.createWallet()
+  }
+
+  showWallets() {
+    this.walletService.getAllWallets()
+  }
 }
