069de0e6d0d069e18b9cf275303fea0fcbac6289
Esteban <estebanarivasv@gmail.com>
2021-06-20 12:32:27 -0300

    Wallets creation working

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
 import { TransactionModel } from '../models/transaction.model';
+import { WalletModel } from '../models/wallet.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
 
-  constructor() { }
-  
+  wallets: Array<WalletModel> = []
+
   getWalletData() { }
-  
+
   // TODO: Validate if wallet name exists.
-  createWallet() { }
-  
+  createWallet() {
+    let wallet1 = new WalletModel(1, "Fixed savings", [], "0x54a5sd45as4d", 560042.2);
+    let wallet2 = new WalletModel(2, "Normal savings", [], "0x54a5as66as4d", 486.2);
+    let wallets = [wallet1, wallet2]
+
+    window.localStorage.setItem('wallets', JSON.stringify(wallets))
+  }
+
   deleteWallet() { }
-  
+
   addTransaction(transaction: TransactionModel) { }
-  
+
   modifyTransaction(index: number) { }
-  
+
   deleteTransaction(index: number) { }
 
-  getAllWallets() {}
-  
+  getAllWallets() {
+    let walletsFromLocal = window.localStorage.getItem("wallets")
+
+    if (walletsFromLocal !== null) {
+      this.wallets = JSON.parse(walletsFromLocal)
+      console.log("wallets in localStorage", this.wallets)
+    }
+
+  }
+
 }
