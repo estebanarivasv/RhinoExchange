0acca38fa8b77ba4efc3d0eb62a91f2e4adffe44
Esteban <estebanarivasv@gmail.com>
2021-06-21 20:44:34 -0300

    General CRUD methods working

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -7,20 +7,56 @@ import { WalletModel } from '../models/wallet.model';
 })
 export class WalletService {
 
-  wallets: Array<WalletModel> = []
-
   getWalletData() { }
 
   // TODO: Validate if wallet name exists.
-  createWallet() {
-    let wallet1 = new WalletModel(1, "Fixed savings", [], "0x54a5sd45as4d", 560042.2);
-    let wallet2 = new WalletModel(2, "Normal savings", [], "0x54a5as66as4d", 486.2);
-    let wallets = [wallet1, wallet2];
+  createWallet(newWallet: WalletModel) {
+    let wallets = this.getAllWallets();
+    if (wallets !== null) {
+      newWallet.id = wallets.length;
 
-    window.localStorage.setItem("wallets", JSON.stringify(wallets));
+      console.log("Antes", this.getAllWallets());
+      wallets.push(newWallet);
+      this.saveWalletsList(wallets);
+      console.log("Despues", this.getAllWallets());
+    }
   }
 
   deleteWallet() { }
+
+  getWalletIndex(walletId: number): number {
+    let wallets = this.getAllWallets()
+    if (wallets !== null) {
+      var index: number = wallets.findIndex(function (wallet) {
+        return wallet.id === walletId;
+      });
+      return index
+    }
+    return -1
+  }
+
+  editWallet(editedWallet: WalletModel) {
+    let wallets = this.getAllWallets()
+
+    if (wallets !== null) {
+      if (editedWallet.id !== undefined) {
+        let index = this.getWalletIndex(editedWallet.id)
+        wallets.splice(index, 1, editedWallet)
+        this.saveWalletsList(wallets)
+      }
+    }
+  }
+
+  saveWalletsList(walletList: Array<WalletModel>) {
+    // TODO DELETE THIS
+    //let wallet1 = new WalletModel("Fixed savings", "0x54a5sd45as4d", 560042.2, [], 1);
+    //let wallet2 = new WalletModel("Normal savings", "0x54a5as66as4d", 486.2, [], 2);
+    //let wallets = [wallet1, wallet2];
+
+    window.localStorage.setItem("wallets", JSON.stringify(walletList));
+
+    console.log("wallets guardadas", JSON.stringify(walletList));
+  }
 
   addTransaction(transaction: TransactionModel) { }
 
@@ -33,8 +69,8 @@ export class WalletService {
     let walletsFromLocal = window.localStorage.getItem("wallets")
 
     if (walletsFromLocal !== null) {
-      this.wallets = JSON.parse(walletsFromLocal)
-      return this.wallets;
+      let wallets = JSON.parse(walletsFromLocal)
+      return wallets;
     } else {
       alert("There are not available wallets")
       return null
