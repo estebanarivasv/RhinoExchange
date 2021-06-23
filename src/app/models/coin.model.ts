77435aa982d3d4956db51f7f59519f2c08c5ffc5
Esteban <estebanarivasv@gmail.com>
2021-06-23 06:22:50 -0300

    Adapting models

--- src/app/models/coin.model.ts
+++ src/app/models/coin.model.ts
@@ -1,28 +1,22 @@
 interface ICoin {
-    id: string,
     symbol: string,
-    name: string
-    image?: {
-        thumb: string
-        small: string
-        large: string
-    },
-    marketData?: {
-        currentPrice: {};
-    }
+    name: string,
+    image: string,
+    current_price: number,
+    id: string
 }
 
 export class CoinModel implements ICoin {
 
     constructor(
-        public id: string,
         public symbol: string,
         public name: string,
-        public image: { thumb: string; small: string; large: string; },
-        public marketData: { currentPrice: {} }
+        public image: string,
+        public current_price: number,
+        public id: string
     ) {
 
     }
-    
+
 
 }
